d531891d4748b26a4e6a77c3d1c9b89daa3c94e0
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-04-29 06:34:08 +0530

    randomised quicksort added

---
+++ 1_Divide And Conquer/P4_RandomisedQuickSort.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i=low-1;
+    int j=low;
+    while(j < high){
+        if(arr[j] < pivot){
+            i++;
+            swap(arr[i], arr[j]);
+        }
+
+        j++;
+    }
+
+    swap(arr[i+1], arr[high]);
+    return i+1;
+}
+
+int randomisedPartition(int arr[], int low, int high){
+    int randumIndex = low + rand() % (high - low + 1);
+    swap(arr[randumIndex], arr[high]);
+    return partition(arr, low, high);
+}
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){
+        int pi = randomisedPartition(arr, low, high);
+        quickSort(arr, low, pi-1);
+        quickSort(arr, pi+1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 80, 30, 90, 40};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i]<< " ";
+    }
+    return 0;
+}
